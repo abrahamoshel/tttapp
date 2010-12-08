5c73f53701e699a8735e5303cf4cd213292470ee
Abraham Oshel <abraham.oshel@gmail.com>
2010-12-08 01:52:00 -0600

    Set up my first test Board.js and BoardSpec.js all test for the Board are passing except diagonal play right to left.

--- js/Board.js
+++ js/Board.js
@@ -1,6 +1,81 @@
 function Board() {
+  this.grid = new Array();
 }
 
 Board.prototype.columns = 3;
 Board.prototype.rows = 3;
-Board.protoype.grid = 3;
+
+Board.prototype.setPosition = function(x, y, player) {
+  if (this.grid[x.toString() + y.toString()] == null) {
+    this.grid[x.toString() + y.toString()] = player;
+    return true;
+  }
+  
+  return false;
+};
+
+Board.prototype.isWinner = function(player) {
+  return this.verticalWinner(player) || this.horizontalWinner(player) || this.diagonalWinner(player);
+}
+
+Board.prototype.verticalWinner = function(player) {
+  for (column = 1; column <= this.columns; column++) {
+    counter = 0;
+
+    for (row = 1; row <= this.rows; row++) {
+      if (this.grid[column.toString() + row.toString()] == player) {
+        counter++;
+      }
+    }
+
+    if (counter == 3) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+Board.prototype.horizontalWinner = function(player) {
+  for (row = 1; row <= this.rows; row++) {
+    counter = 0;
+
+    for (column = 1; column <= this.columns; column++) {
+      if (this.grid[column.toString() + row.toString()] == player) {
+        counter++;
+      }
+    }
+
+    if (counter == 3) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+Board.prototype.diagonalWinner = function(player) {
+  cells = [3, 2, 1];
+
+  for (diag in cells) {
+    if (this.grid[diag.toString() + diag.toString()] == player) {
+      counter++;
+    }
+  }
+
+  if (counter == 3) {
+    return true;
+  }
+
+  cells2 = [13, 22, 31];
+  
+  for (diag in cells2) {
+    if(this.grid[diag.toString()] == player) {
+      counter++;
+    }
+  }
+
+  if (counter == 3) {
+    return true;
+  }
+}
--- js/spec/BoardSpec.js
+++ js/spec/BoardSpec.js
@@ -1,23 +1,109 @@
 describe("Board", function() {
   var board;
 
-  beforeEach(function() {
-    board = new Board();
+  describe("base functionality", function() {
+    beforeEach(function() {
+      board = new Board();
+    });
+
+    it("should have three columns", function() {
+      expect(board.columns).toEqual(3);
+    });
+
+    it("should have three row", function() {
+      expect(board.rows).toEqual(3);
+    });
+
+    it("should have a multi-dimensional grid", function() {
+      expect(board.grid).toBeDefined();
+    });
   });
 
-  it("should have three columns", function() {
-    expect(board.columns).toEqual(3);
+  describe("setting the grid", function() {
+    var player;
+    x = 1;
+    y = 1;
+
+    beforeEach(function() {
+      board = new Board();
+      player = "X";
+    });
+
+    it("should set the grid position if not set", function() {
+      expect(board.setPosition(x, y, player)).toBeTruthy();
+    });
+
+    it("should not set the grid position if already set", function() {
+      board.setPosition(x, y, player);
+      expect(board.setPosition(x, y, player)).toBeFalsy();
+    });
   });
 
-  it("should have three row", function() {
-    expect(board.rows).toEqual(3);
-  });
+  describe("win conditions", function() {
+    var player;
 
-  it("should have a multi-dimensional grid", function() {
-    expect(board.grid).toBeDefined();
-  });
+    beforeEach(function() {
+      board = new Board();
+      player = "X";
+    });
 
-  describe("win condition", function() {
-    
+    it("should win if three in a row vertically exist", function() {
+      board.setPosition(1, 1, player);
+      board.setPosition(1, 2, player);
+      board.setPosition(1, 3, player);
+      expect(board.isWinner(player)).toBeTruthy();
+    });
+
+    it("should win if any three in a row vertically exist", function() {
+      board.setPosition(3, 1, player);
+      board.setPosition(3, 2, player);
+      board.setPosition(3, 3, player);
+      expect(board.isWinner(player)).toBeTruthy();
+    });
+
+    it("it should win if any three in a row vertically exist while other positions exist too", function() {
+      board.setPosition(1, 1, player);
+      board.setPosition(3, 1, player);
+      board.setPosition(3, 2, player);
+      board.setPosition(3, 3, player);
+      expect(board.isWinner(player)).toBeTruthy();
+    });
+
+    it("should win if three in a row horizontally exist", function() {
+      board.setPosition(1, 1, player);
+      board.setPosition(2, 1, player);
+      board.setPosition(3, 1, player);
+      expect(board.isWinner(player)).toBeTruthy();
+    });
+
+    it("should win if any three in a row horizontally exist", function() {
+      board.setPosition(1, 3, player);
+      board.setPosition(2, 3, player);
+      board.setPosition(3, 3, player);
+      expect(board.isWinner(player)).toBeTruthy();
+    });
+
+    it("should win if any three in a row horizontally exist while other positions exist too", function() {
+      board.setPosition(1, 1, player);
+      board.setPosition(1, 3, player);
+      board.setPosition(2, 3, player);
+      board.setPosition(3, 3, player);
+      expect(board.isWinner(player)).toBeTruthy();
+    });
+
+    it("should win if any three in a row diagonally left to right exist", function() {
+      board.setPosition(1, 1, player);
+      board.setPosition(2, 2, player);
+      board.setPosition(3, 3, player);
+      expect(board.isWinner(player)).toBeTruthy();
+    });
+
+    it("should win if any three in a row diagonally right to left exist", function() {
+      board.setPosition(1, 3, player);
+      board.setPosition(2, 2, player);
+      board.setPosition(3, 1, player);
+      expect(board.isWinner(player)).toBeTruthy();
+    });
+
   });
 });
